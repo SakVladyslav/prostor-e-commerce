7ede2f2699408f6180ea6d2f3777ea4292a9518e
vladyslav sak <vladyslav.sak@gmail.com>
2025-12-24 18:33:22 +0200

    added card components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import sampleData from '@/sample-data';
+
+import ProductList from '@/components/shared/product/product-list';
+
 const Homepage = () => {
-  return <span>Prostor</span>;
+  return (
+    <>
+      <ProductList
+        title="Featured Products"
+        data={sampleData.products}
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
