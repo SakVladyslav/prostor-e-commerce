4e0d0193f742e538f077c26dea7c7f37fa136b97
vladyslav sak <vladyslav.sak@gmail.com>
2026-02-26 15:13:03 +0200

    added filters drawer

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 import { getAllProducts } from '@/lib/actions/product.actions';
 
 import ProductCard from '@/components/shared/product/product-card';
+import { Button } from '@/components/ui/button';
+
+import SearchFilters from './search-filters';
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -21,6 +26,31 @@ const SearchPage = async (props: {
     sort = 'newest',
   } = await props.searchParams;
 
+  const params = { q, sort, rating, price, page, category };
+
+  // Construct filter url
+  const getFilterUrl = ({
+    c,
+    s,
+    p,
+    r,
+    pg,
+  }: {
+    c?: string;
+    s?: string;
+    p?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    if (c) params.category = c;
+    if (s) params.sort = s;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   const { data, totalPages } = await getAllProducts({
     page: Number(page),
     query: q,
@@ -32,9 +62,26 @@ const SearchPage = async (props: {
 
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* FILTERS */}</div>
+      <SearchFilters {...params} cbFilter={getFilterUrl} />
 
       <div className="md:col-span-4 space-y-4">
+        <div className="flex-between flex-col my-4 md:flex-row">
+          <div className="flex items-center">
+            {q !== 'all' && q !== '' && 'Query: ' + q}
+            {category !== 'all' && category !== '' && ' Category: ' + category}
+            {price !== 'all' && ' Price: ' + price}
+            {rating !== 'all' && ' Rating: ' + rating + ' stars & up'}
+            &nbsp;
+            {(q !== 'all' && q !== '') ||
+            (category !== 'all' && category !== '') ||
+            rating !== 'all' ||
+            price !== 'all' ? (
+              <Button variant="link" asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+        </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {data.length === 0 && <div>No products found.</div>}
           {data.map((product) => (
---
+++ app/(root)/search/search-filters.tsx
@@ -0,0 +1,98 @@
+import Link from 'next/link';
+
+import { getAllProductCategories } from '@/lib/actions/product.actions';
+import { prices, ratings } from '@/lib/constants/search-filters';
+
+type SearchFiltersProps = {
+  q?: string;
+  category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
+  page?: string;
+  cbFilter: (c: any) => string;
+};
+
+const SearchFilters = async (props: SearchFiltersProps) => {
+  let { category, page, price, q, rating, sort, cbFilter } = props;
+
+  const categories = await getAllProductCategories();
+
+  return (
+    <div className="filter-links">
+      <div className="text-xl mb-2 mt-3">Department</div>
+      <div>
+        <ul className="space-y-1">
+          <li>
+            <Link
+              className={`${(category === 'all' || category === '') && 'font-bold'}`}
+              href={cbFilter({ c: 'all' })}
+            >
+              Any
+            </Link>
+          </li>
+          {categories.map((x) => (
+            <li key={x.category}>
+              <Link
+                className={`${category === x.category && 'font-bold'}`}
+                href={cbFilter({ c: x.category })}
+              >
+                {x.category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="text-xl mb-2 mt-8">Price</div>
+      <div>
+        <ul className="space-y-1">
+          <li>
+            <Link
+              className={`${(price === 'all' || price === '') && 'font-bold'}`}
+              href={cbFilter({ p: 'all' })}
+            >
+              Any
+            </Link>
+          </li>
+          {prices.map((x) => (
+            <li key={x.value}>
+              <Link
+                className={`${price === x.value && 'font-bold'}`}
+                href={cbFilter({ p: x.value })}
+              >
+                {x.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="text-xl mb-2 mt-8">Customer Ratings</div>
+      <div>
+        <ul className="space-y-1">
+          <li>
+            <Link
+              className={`${(rating === 'all' || rating === '') && 'font-bold'}`}
+              href={cbFilter({ r: 'all' })}
+            >
+              Any
+            </Link>
+          </li>
+          {ratings.map((rate) => (
+            <li key={rate}>
+              <Link
+                className={`${rating === rate.toString() && 'font-bold'}`}
+                href={cbFilter({ r: rate.toString() })}
+              >
+                {`${rate} stars & up`}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SearchFilters;
